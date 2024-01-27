15a661d26fb10ffba5ec5b585e07e999864ee8dc
Chaibouu <chaibouabdoulwahab@gmail.com>
2024-01-27 23:52:38 +0100

    ad signin

--- components/SignIn/SignIn.jsx
+++ components/SignIn/SignIn.jsx
@@ -7,7 +7,7 @@ const SignIn = ({closeModal}) => {
     return (
         <>
             <div onClick={closeModal} className='z-[30] fixed inset-0 bg-slate-800/75'></div>
-            {/* <div className="fixed z-[40] top-1/3 left-1/2 -translate-y-1/2 -translate-x-1/2">
+            <div className="fixed z-[40] top-1/3 left-1/2 -translate-y-1/2 -translate-x-1/2">
             <div className="modal-content">
                 <h2>Connexion</h2>
                 <label>{`Nom d'utilisateur`}:</label>
@@ -16,7 +16,7 @@ const SignIn = ({closeModal}) => {
                 <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 <button >Se connecter</button>
             </div>
-            </div> */}
+            </div>
         </>
     );
 };

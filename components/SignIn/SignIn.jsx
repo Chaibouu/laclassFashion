9507674e767dea67fa046700dab40d59412f82d2
Chaibouu <chaibouabdoulwahab@gmail.com>
2024-01-27 23:53:44 +0100

    add

--- components/SignIn/SignIn.jsx
+++ components/SignIn/SignIn.jsx
@@ -23,6 +23,7 @@ const SignIn = ({closeModal}) => {
             {/* commentaire */}
             {/* commentaire */}
             {/* commentaire */}
+            {/* commentaire */}
         </>
     );
 };

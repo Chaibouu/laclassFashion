31d5144f540d756e01406218ddf76ee556672dfa
Chaibouu <chaibouabdoulwahab@gmail.com>
2024-01-28 01:34:27 +0100

    add

--- components/SignIn/SignIn.jsx
+++ components/SignIn/SignIn.jsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React, { useState } from 'react';
 
 const SignIn = ({closeModal}) => {
@@ -7,13 +9,19 @@ const SignIn = ({closeModal}) => {
     return (
         <>
             <div onClick={closeModal} className='z-[30] fixed inset-0 bg-slate-800/75'></div>
-            <div className="fixed z-[40] top-1/3 left-1/2 -translate-y-1/2 -translate-x-1/2">
+            <div className="fixed z-[40] top-1/3 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-white/75 text-slate-900">
             <div className="modal-content">
+                <Image src="/Logo/laclassFashionBlack.png" height={900} width={900} alt='Laclass Fashion Couture' />
                 <h2>Connexion</h2>
-                <label>{`Nom d'utilisateur`}:</label>
-                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-                <label>Mot de passe:</label>
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <div>
+                    <label>{`Nom d'utilisateur`}</label>
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+                </div>
+                <div>
+                    <label>Mot de passe</label>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </div>
+                <div><Link href="/"></Link></div>
                 <button >Se connecter</button>
             </div>
             </div>

ce0ede0a6dba569265d039ea0ac36636ad80b25f
Chaibouu <chaibouabdoulwahab@gmail.com>
2024-02-09 20:26:34 +0100

    add

--- app/page.jsx
+++ app/page.jsx
@@ -63,6 +63,7 @@ export default function Home() {
           {/* commentaire */}
           {/* commentaire */}
           {/* commentaire */}
+          {/* commentaire */}
         </div>
     </>
   );

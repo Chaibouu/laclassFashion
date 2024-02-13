13b81a5c3682951653fd42a8b9bb73d959540afa
Chaibouu <chaibouabdoulwahab@gmail.com>
2024-02-13 13:46:34 +0100

    add

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -4,14 +4,17 @@ import React from 'react';
 const About = () => {
     return (
         <>
-             <div className='bg-slate-700'>
-              <p> <Image src="/Logo/laclassFashion.png" className='w-[300px]' height={900} width={900} alt="" />Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+             <div className='bg-slate-700 flex'>
+              <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                 Accusantium autem saepe omnis ipsum asperiores. Totam nihil
                 dolorum ea modi culpa porro omnis cupiditate neque consequuntur,
                 quis recusandae, fuga perspiciatis quam, corrupti cum corporis
                 numquam nostrum fugiat beatae delectus cumque quaerat? Dolore
                 recusandae inventore corporis laudantium beatae consequatur nemo
-                nostrum delectus!Lorem ipsum dolor sit amet consectetur, adipisicing elit.Accusantium autem saepe omnis ipsum asperiores. Totam nihil dolorum ea modi culpa porro omnis cupiditate neque consequuntur!</p>
+                nostrum delectus!Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Accusantium autem saepe omnis ipsum asperiores. Totam nihil dolorum ea modi culpa porro 
+                omnis cupiditate neque consequuntur!</p>
+                <Image src="/Logo/laclassFashion.png" className='w-[300px]' height={900} width={900} alt="" />
             </div>
         </>
     );
